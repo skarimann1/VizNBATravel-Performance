f768af0a2b77d33d0b71e8e5ba11ae4c2ad02f5f
johndrab <john.drab@yahoo.com>
2026-03-08 15:04:57 -0700

    basic netRating vs Fatigure index plot with team filters

--- ReactVizGeneration/src/App.tsx
+++ ReactVizGeneration/src/App.tsx
@@ -1,11 +1,15 @@
 import NBATravelMap from "./components/NbaTravelMap";
 import NBATravelMap_v2 from "./components/NbaTravelMap_v2";
 import NBATravelMap_v3 from "./components/NbaTravelMap_v3";
+import NBATravelMap_v4 from "./components/NbaTravelMap_v4";
+
+import FatigueScatterPlot from "./components/FatigueScatterPlot";
 
 function App() {
   return (
     <div>
       <NBATravelMap_v3 />
+      <FatigueScatterPlot />
     </div>
   );
 }
